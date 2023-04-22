9d0f4e20e0d121eabffa0bb44eb8c7e6149519df
Matthijs Steen <emailmatthijs@gmail.com>
2023-04-22 13:58:24 +0200

    refactor: test inputs should remain outside the main flake

--- flake.nix
+++ flake.nix
@@ -2,13 +2,7 @@
   description = "NixOS configuration library";
 
   inputs = {
-    # nixpkgs.url = "github:nix-community/nixpkgs.lib/44214417fe4595438b31bdb9469be92536a61455";
     nixpkgs.url = "github:NixOS/nixpkgs/e2c97799da5f5cd87adfa5017fba971771e123ef";
-
-    nixos-unstable.url = "github:NixOS/nixpkgs/19cf008bb18e47b6e3b4e16e32a9a4bdd4b45f7e";
-
-    extra-container.url = "github:erikarvstedt/extra-container";
-    extra-container.inputs.nixpkgs.follows = "nixos-unstable";
   };
 
   outputs = {
@@ -30,18 +24,5 @@
       if results == [ ]
       then null
       else results;
-
-    testNixcfg = let
-      self = nixcfg.lib.mkNixcfg {
-        name = "test";
-        path = ./test/lib/nixcfg;
-        inputs =
-          inputs
-          // {
-            inherit self;
-          };
-      };
-    in
-      self;
   };
 }
